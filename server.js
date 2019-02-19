0d599abc926c51d84c4c3119bf99e1bf3808c2fc
himanshush200599 <himanshush214@gmail.com>
2019-02-19 00:59:15 -0500

    Added some more dependencies and set middleware

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const ejsMate = require("ejs-mate");
+
 const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+
+mongoose.connect(
+  "mongodb://root:abc123@ds141815.mlab.com:41815/amazon-clone",
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("MongoDB is connected");
+    }
+  }
+);
 
 app.get("/", (req, res) => {
   res.json({
