0485f341828618c4fe4b2906266e332683dbbb1c
himanshush200599 <himanshush214@gmail.com>
2019-02-22 18:25:09 -0500

    refactor code

--- models/product.js
+++ models/product.js
@@ -1,13 +1,10 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const Category = require("./category");
-let ProductSchema = new Schema({
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: "Category"
-  },
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var ProductSchema = new Schema({
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
   name: String,
   price: Number,
   image: String
 });
+
 module.exports = mongoose.model("Product", ProductSchema);
